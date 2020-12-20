6759d0017ee0634dae9ef88396ff76987d0f08c4
legendawdt <75623072+legendawdt@users.noreply.github.com>
2020-12-20 14:13:24 +0500

    Update ss-vpn.sh

--- ss-vpn.sh
+++ ss-vpn.sh
@@ -181,3 +181,219 @@ Set_port(){
 		fi
 		done
 }
+Set_password(){
+	echo "请输入 Shadowsocks 密码 [0-9][a-z][A-Z]"
+	read -e -p "(默认: 随机生成):" ss_password
+	[[ -z "${ss_password}" ]] && ss_password=$(date +%s%N | md5sum | head -c 16)
+	echo && echo "========================"
+	echo -e "	密码 : ${Red_background_prefix} ${ss_password} ${Font_color_suffix}"
+	echo "========================" && echo
+}
+Set_cipher(){
+	echo -e "请选择 Shadowsocks 加密方式
+	
+ ${Green_font_prefix} 1.${Font_color_suffix} aes-128-cfb
+ ${Green_font_prefix} 2.${Font_color_suffix} aes-128-ctr
+ ${Green_font_prefix} 3.${Font_color_suffix} aes-192-cfb
+ ${Green_font_prefix} 4.${Font_color_suffix} aes-192-ctr
+ ${Green_font_prefix} 5.${Font_color_suffix} aes-256-cfb
+ ${Green_font_prefix} 6.${Font_color_suffix} aes-256-ctr
+ ${Green_font_prefix} 7.${Font_color_suffix} chacha20-ietf
+ ${Green_font_prefix} 8.${Font_color_suffix} xchacha20
+ ${Green_font_prefix} 9.${Font_color_suffix} aes-128-gcm            (AEAD)
+ ${Green_font_prefix}10.${Font_color_suffix} aes-192-gcm            (AEAD)
+ ${Green_font_prefix}11.${Font_color_suffix} aes-256-gcm            (AEAD)
+ ${Green_font_prefix}12.${Font_color_suffix} chacha20-ietf-poly1305 (AEAD)
+
+${Tip} chacha20 系列加密方式无需额外安装 libsodium，Shadowsocks Go版默认集成 !" && echo
+	read -e -p "(默认: 12. chacha20-ietf-poly1305):" ss_cipher
+	[[ -z "${ss_cipher}" ]] && ss_cipher="12"
+	if [[ ${ss_cipher} == "1" ]]; then
+		ss_cipher="aes-128-cfb"
+	elif [[ ${ss_cipher} == "2" ]]; then
+		ss_cipher="aes-128-ctr"
+	elif [[ ${ss_cipher} == "3" ]]; then
+		ss_cipher="aes-192-cfb"
+	elif [[ ${ss_cipher} == "4" ]]; then
+		ss_cipher="aes-192-ctr"
+	elif [[ ${ss_cipher} == "5" ]]; then
+		ss_cipher="aes-256-cfb"
+	elif [[ ${ss_cipher} == "6" ]]; then
+		ss_cipher="aes-256-ctr"
+	elif [[ ${ss_cipher} == "7" ]]; then
+		ss_cipher="chacha20-ietf"
+	elif [[ ${ss_cipher} == "8" ]]; then
+		ss_cipher="xchacha20"
+	elif [[ ${ss_cipher} == "9" ]]; then
+		ss_cipher="aead_aes_128_gcm"
+	elif [[ ${ss_cipher} == "10" ]]; then
+		ss_cipher="aead_aes_192_gcm"
+	elif [[ ${ss_cipher} == "11" ]]; then
+		ss_cipher="aead_aes_256_gcm"
+	elif [[ ${ss_cipher} == "12" ]]; then
+		ss_cipher="aead_chacha20_poly1305"
+	else
+		ss_cipher="aead_chacha20_poly1305"
+	fi
+	echo && echo "========================"
+	echo -e "	加密 : ${Red_background_prefix} ${ss_cipher} ${Font_color_suffix}"
+	echo "========================" && echo
+}
+Set_verbose(){
+	echo -e "是否启用详细日志模式？[Y/n]
+启用详细日志模式就可以在日志中看到链接者信息(链接时间、链接代理端口、链接者IP、链接者访问的目标域名或IP这些非敏感类信息)。"
+	read -e -p "(默认：N 禁用):" ss_verbose
+	[[ -z "${ss_verbose}" ]] && ss_verbose="N"
+	if [[ "${ss_verbose}" == [Yy] ]]; then
+		ss_verbose="YES"
+	else
+		ss_verbose="NO"
+	fi
+	echo && echo "========================"
+	echo -e "	详细日志模式 : ${Red_background_prefix} ${ss_verbose} ${Font_color_suffix}"
+	echo "========================" && echo
+}
+Set(){
+	check_installed_status
+	echo && echo -e "你要做什么？
+ ${Green_font_prefix}1.${Font_color_suffix}  修改 端口配置
+ ${Green_font_prefix}2.${Font_color_suffix}  修改 密码配置
+ ${Green_font_prefix}3.${Font_color_suffix}  修改 加密配置
+ ${Green_font_prefix}4.${Font_color_suffix}  修改 详细日志模式 配置
+ ${Green_font_prefix}5.${Font_color_suffix}  修改 全部配置
+————————————————
+ ${Green_font_prefix}6.${Font_color_suffix}  监控 运行状态" && echo
+	read -e -p "(默认: 取消):" ss_modify
+	[[ -z "${ss_modify}" ]] && echo "已取消..." && exit 1
+	if [[ "${ss_modify}" == "1" ]]; then
+		Read_config
+		Set_port
+		ss_password=${password}
+		ss_cipher=${cipher}
+		ss_verbose=${verbose}
+		Write_config
+		Del_iptables
+		Add_iptables
+		Restart
+	elif [[ "${ss_modify}" == "2" ]]; then
+		Read_config
+		Set_password
+		ss_port=${port}
+		ss_cipher=${cipher}
+		ss_verbose=${verbose}
+		Write_config
+		Restart
+	elif [[ "${ss_modify}" == "3" ]]; then
+		Read_config
+		Set_cipher
+		ss_port=${port}
+		ss_password=${password}
+		ss_verbose=${verbose}
+		Write_config
+		Restart
+	elif [[ "${ss_modify}" == "4" ]]; then
+		Read_config
+		Set_verbose
+		ss_port=${port}
+		ss_password=${password}
+		ss_cipher=${cipher}
+		Write_config
+		Restart
+	elif [[ "${ss_modify}" == "5" ]]; then
+		Read_config
+		Set_port
+		Set_password
+		Set_cipher
+		Set_verbose
+		Write_config
+		Restart
+	elif [[ "${ss_modify}" == "6" ]]; then
+		Set_crontab_monitor
+	else
+		echo -e "${Error} 请输入正确的数字(1-6)" && exit 1
+	fi
+}
+Install(){
+	check_root
+	[[ -e ${FILE} ]] && echo -e "${Error} 检测到 Shadowsocks 已安装 !" && exit 1
+	echo -e "${Info} 开始设置 用户配置..."
+	Set_port
+	Set_password
+	Set_cipher
+	Set_verbose
+	echo -e "${Info} 开始安装/配置 依赖..."
+	Installation_dependency
+	echo -e "${Info} 开始下载/安装..."
+	check_new_ver
+	Download
+	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
+	Service
+	echo -e "${Info} 开始写入 配置文件..."
+	Write_config
+	echo -e "${Info} 开始设置 iptables防火墙..."
+	Set_iptables
+	echo -e "${Info} 开始添加 iptables防火墙规则..."
+	Add_iptables
+	echo -e "${Info} 开始保存 iptables防火墙规则..."
+	Save_iptables
+	echo -e "${Info} 所有步骤 安装完毕，开始启动..."
+	Start
+}
+Start(){
+	check_installed_status
+	check_pid
+	[[ ! -z ${PID} ]] && echo -e "${Error} Shadowsocks 正在运行，请检查 !" && exit 1
+	/etc/init.d/ss-go start
+	#sleep 1s
+	check_pid
+	[[ ! -z ${PID} ]] && View
+}
+Stop(){
+	check_installed_status
+	check_pid
+	[[ -z ${PID} ]] && echo -e "${Error} Shadowsocks 没有运行，请检查 !" && exit 1
+	/etc/init.d/ss-go stop
+}
+Restart(){
+	check_installed_status
+	check_pid
+	[[ ! -z ${PID} ]] && /etc/init.d/ss-go stop
+	/etc/init.d/ss-go start
+	#sleep 1s
+	check_pid
+	[[ ! -z ${PID} ]] && View
+}
+Update(){
+	check_installed_status
+	check_new_ver
+	check_ver_comparison
+}
+Uninstall(){
+	check_installed_status
+	echo "确定要卸载 Shadowsocks ? (y/N)"
+	echo
+	read -e -p "(默认: n):" unyn
+	[[ -z ${unyn} ]] && unyn="n"
+	if [[ ${unyn} == [Yy] ]]; then
+		check_pid
+		[[ ! -z $PID ]] && kill -9 ${PID}
+		if [[ -e ${CONF} ]]; then
+			port=$(cat ${CONF}|grep 'PORT = '|awk -F 'PORT = ' '{print $NF}')
+			Del_iptables
+			Save_iptables
+		fi
+		if [[ ! -z $(crontab -l | grep "ss-go.sh monitor") ]]; then
+			crontab_monitor_cron_stop
+		fi
+		rm -rf "${FOLDER}"
+		if [[ ${release} = "centos" ]]; then
+			chkconfig --del ss-go
+		else
+			update-rc.d -f ss-go remove
+		fi
+		rm -rf "/etc/init.d/ss-go"
+		echo && echo "Shadowsocks 卸载完成 !" && echo
+	else
+		echo && echo "卸载已取消..." && echo
+	fi
+}
